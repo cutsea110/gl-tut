acb1536a74acf4e1411050ce743f849f5ec98ef0
cutsea110 <cutsea110@gmail.com>
2012-07-02 23:00:22 +0900

    Set color for each vertex

--- prog1.hs
+++ prog1.hs
@@ -6,12 +6,16 @@ import Graphics.UI.GLUT
 display :: IO ()
 display = do
   clear [ColorBuffer]
-  color red
-  renderPrimitive Polygon $ mapM_ vertex vs
+  renderPrimitive Polygon $ mapM_ draw $ zip colors vs
   flush
   where
-    red :: Color3 GLdouble
-    red = Color3 1.0 0.0 0.0
+    draw (c,v) = color c >> vertex v
+    colors :: [Color3 GLdouble]
+    colors = [ Color3 1.0 0.0 0.0
+             , Color3 0.0 1.0 0.0
+             , Color3 0.0 0.0 1.0
+             , Color3 1.0 1.0 0.0
+             ]
     vs :: [Vertex2 GLfloat]
     vs = [ Vertex2 (-0.9) (-0.9)
          , Vertex2 0.9 (-0.9)
