278010c1fd3b3af1005c07624457a65312cfb170
cutsea110 <cutsea110@gmail.com>
2012-07-02 21:54:15 +0900

    paint window

--- prog1.hs
+++ prog1.hs
@@ -1,13 +1,22 @@
 module Main where
 
+import Prelude hiding (init)
 import Graphics.UI.GLUT
 
 display :: IO ()
-display = return ()
+display = do
+  clear [ColorBuffer]
+  flush
+
+
+init :: IO ()
+init = clearColor $= Color4 0.0 0.0 1.0 1.0
 
 main :: IO ()
 main = do
   (progName, _) <- getArgsAndInitialize
+  initialDisplayMode $= [RGBAMode]
   createWindow progName
   displayCallback $= display
+  init
   mainLoop
