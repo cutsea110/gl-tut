1f18dc4bf08a098c3760888ede20fb70120c69aa
cutsea110 <cutsea110@gmail.com>
2012-07-04 18:44:01 +0900

    Display rubberband

--- prog1.hs
+++ prog1.hs
@@ -3,11 +3,12 @@ module Main where
 import Prelude hiding (init)
 import Graphics.UI.GLUT
 import Data.IORef
+import Data.Maybe
 
 display :: IORef [Position] -> IO ()
-display r = do
+display rps = do
   clear [ColorBuffer]
-  ps <- readIORef r
+  ps <- readIORef rps
   renderPrimitive Lines $ do
     color $ Color3 0.0 0.0 (0.0::GLdouble)
     mapM_ vertex2D $ reverse ps
@@ -24,22 +25,24 @@ resize s@(Size w h) = do
   where
     (w', h') = (fromIntegral w, fromIntegral h)
 
-keymouse :: IORef [Position] -> KeyboardMouseCallback
-keymouse r (MouseButton LeftButton) Down _ p = do
-  modifyIORef r (p:)
+keymouse :: IORef [Position] -> IORef (Maybe Position) -> KeyboardMouseCallback
+keymouse rps rp (MouseButton LeftButton) Down _ p = do
+  modifyIORef rps (p:)
+  writeIORef rp (Just p)
   putStr "down at "
   putStrLn $ show p
-keymouse r (MouseButton LeftButton) Up _ p = do
-  modifyIORef r (p:)
+keymouse rps rp (MouseButton LeftButton) Up _ p = do
+  (p0:_) <- readIORef rps
+  modifyIORef rps (p:)
+  writeIORef rp Nothing
   putStr "up at "
   putStrLn $ show p
-  (p1:p0:_) <- readIORef r
   renderPrimitive Lines $ do
     color $ Color3 0.0 0.0 (0.0::GLdouble)
     vertex2D p0
-    vertex2D p1
+    vertex2D p
   flush
-keymouse _ _ _ _ _ = return ()
+keymouse _ _ _ _ _ _ = return ()
 
 vertex2D :: Position -> IO ()
 vertex2D (Position x y) = vertex2f $ Vertex2 x' y'
@@ -47,25 +50,48 @@ vertex2D (Position x y) = vertex2f $ Vertex2 x' y'
     (x', y') = (fromIntegral x, fromIntegral y)
     vertex2f = vertex :: Vertex2 GLfloat -> IO ()
 
-mouse :: IORef [Position] -> MotionCallback
-mouse _ = print
-
+mouse :: IORef [Position] -> IORef (Maybe Position) -> MotionCallback
+mouse rps rp p = do
+  (p0:_) <- readIORef rps
+  mp <- readIORef rp
+  writeIORef rp (Just p)
+  logicOp $= Just Invert
+  if isJust mp
+    then do
+    renderPrimitive Lines $ do
+      color $ Color3 0.0 0.0 (0.0::GLdouble)
+      vertex2D p0
+      vertex2D $ fromJust mp
+    flush
+    else
+    return ()
+  logicOp $= Just Copy
+  ps <- readIORef rps
+  renderPrimitive Lines $ do
+    color $ Color3 0.0 0.0 (0.0::GLdouble)
+    vertex2D p0
+    vertex2D p
+    mapM_ vertex2D $ reverse ps
+  flush
+  
 init :: IO ()
 init = clearColor $= Color4 1.0 1.0 1.0 1.0
 
 main :: IO ()
 main = do
   -- trace mouse position
-  ref <- newIORef []
+  rps <- newIORef []
+  -- cache previous mouse position
+  rp <- newIORef Nothing
   
   initialWindowPosition $= Position 100 100
   initialWindowSize $= Size 320 240
   (progName, _) <- getArgsAndInitialize
   initialDisplayMode $= [RGBAMode]
   createWindow progName
-  displayCallback $= display ref
+  displayCallback $= display rps
   reshapeCallback $= Just resize
-  keyboardMouseCallback $= Just (keymouse ref)
-  motionCallback $= Just (mouse ref)
+  keyboardMouseCallback $= Just (keymouse rps rp)
+  motionCallback $= Just (mouse rps rp)
   init
   mainLoop
